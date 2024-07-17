64afbeb8dd247db2f97149f740b646b980c89bbf
Amadej Kastelic <amadejkastelic7@gmail.com>
2024-07-17 23:40:02 +0200

    Update nix shell

--- Makefile
+++ Makefile
@@ -9,3 +9,5 @@ lint:
 	python -m flake8 **/*.py
 format:
 	python -m black **/*.py
+nix-env:
+	@nix-shell --command 'source "$$(pipenv --venv)/bin/activate"; return'
--- shell.nix
+++ shell.nix
@@ -1,11 +1,23 @@
 {pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable") {}}:
-pkgs.mkShellNoCC {
+pkgs.mkShell {
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
   packages = with pkgs; [
     python312
     python312Packages.pip
+    python312Packages.python-magic
+    python312Packages.playwright
+    playwright
+    playwright-driver.browsers
     docker-compose
     pipenv
     curl
     jq
+    file
   ];
+
+  shellHook = ''
+    export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+    export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+  '';
 }
