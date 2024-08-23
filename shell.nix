d733531b675953095fceeafb8e25c97794e64a87
Amadej Kastelic <amadejkastelic7@gmail.com>
2024-08-23 09:35:31 +0200

    Fix nix shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,22 @@
-{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11") {}}:
-pkgs.mkShellNoCC {
+{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable") {}}:
+pkgs.mkShell {
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
   packages = with pkgs; [
-    python3
+    python312
+    python312Packages.pip
+    python312Packages.python-magic
+    playwright
+    playwright-driver.browsers
+    docker-compose
     pipenv
     curl
     jq
+    file
   ];
+
+  shellHook = ''
+    export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+    export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+  '';
 }
