53eebf05e6eb011c700fcbdff7f80ed5a16f8bc6
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-04-19 13:59:37 +0200

    facebook: Add additional headers and expose them to settings (#44)
    * facebook: Add additional headers and expose them to settings
    
    * uv: upgrade

--- bot/integrations/facebook/client.py
+++ bot/integrations/facebook/client.py
@@ -15,10 +15,17 @@ from bot.integrations.facebook import config
 
 _API_URL = 'https://fdown.net/'
 _HEADERS = {
-    'User-Agent': fake_useragent.UserAgent().random,
+    'User-Agent': fake_useragent.UserAgent().chrome,
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
     'Accept-Language': 'en-US,en;q=0.9',
     'Referer': 'https://fdown.net/',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Alt-Used': 'fdown.net',
+    'Host': 'fdown.net',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
 }
 
 
@@ -35,14 +42,15 @@ class FacebookClientSingleton(base.BaseClientSingleton):
             cls._INSTANCE = base.MISSING
             return
 
-        cls._INSTANCE = FacebookClient()
+        cls._INSTANCE = FacebookClient(headers=conf.headers)
 
 
 class FacebookClient(base.BaseClient):
     INTEGRATION = constants.Integration.FACEBOOK
 
-    def __init__(self):
+    def __init__(self, headers: typing.Optional[typing.Dict[str, str]] = None) -> None:
         super().__init__()
+        self.headers = _HEADERS | (headers or {})
 
     async def get_integration_data(self, url: str) -> typing.Tuple[constants.Integration, str, typing.Optional[int]]:
         if '/watch' in url.split('?')[0] and 'v=' in url:
@@ -51,7 +59,7 @@ class FacebookClient(base.BaseClient):
         return self.INTEGRATION, url.strip('/').split('?')[0].split('/')[-1], None
 
     async def get_post(self, url: str) -> domain.Post:
-        async with aiohttp.ClientSession(headers=_HEADERS) as session:
+        async with aiohttp.ClientSession(headers=self.headers) as session:
             # Set cookies
             async with session.get(_API_URL) as response:
                 pass
